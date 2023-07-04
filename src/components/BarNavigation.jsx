5637c0b7f52bda46bf5e001c3b102317d80169d0
RosinskaK <rosinska.eu@gmail.com>
2023-07-04 12:56:04 +0200

    nav bar styling part 1

--- src/components/BarNavigation.jsx
+++ src/components/BarNavigation.jsx
@@ -1,8 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
 
 function BarNavigation () {
 
+    const [show, setShow] = useState(false);
+
+    useEffect(() =>{
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                setShow(true); 
+            } else setShow(false);
+        });
+        return () => {
+            window.removeEventListener('scroll');
+        };
+    }, []);
+
     return (
-        <div className="nav-main-container">
+        <div className={`nav-main-container ${show && 'nav-black'}`}>
             <div className="nav-left-icons">
                 <h1 className="nav-logo">MOV<span>i</span>E<span>on</span></h1>
                 <button className="nav-left-icon">Strona głowna</button>
