5fe557cb233a7264279b0256d5990d03fbbe52e0
RosinskaK <rosinska.eu@gmail.com>
2024-01-09 12:33:30 +0100

    adding in Search component btn clear input text

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,37 +9,55 @@ function Search() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
 
+  const [showClearButton, setShowClearButton] = useState(false);
+
+
   const onChange = (e) => {
     e.preventDefault();
 
-    setQuery(e.target.value);
+  const inputText = e.target.value;
+  setQuery(inputText);
 
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
-    .then( (res) => res.json())
-    .then((data) => {
-      if (!data.errors) {
-        setResults(data.results);
-      } else {
-        setResults([]);
-      }
-    });
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1&include_adult=false&query=${inputText}`)
+        .then((res) => res.json())
+        .then((data) => {
+            if (!data.errors) {
+                setResults(data.results);
+                setShowClearButton(inputText.length > 0);
+            } else {
+                setResults([]);
+            }
+        });
   };
 
   //console.log(results);
   //console.log(query);
 
+
+  const clearInput = () => {
+      setQuery("");
+      setResults([]);
+      setShowClearButton(false);
+  };
+
+  
   return (
      <div className='search-main'>
         <BarNavigation />
         <div className='search-container'>
           <div className='search-input'>
             <input 
-            name='search'
-            type='text' 
-            placeholder='Szukaj filmów'
-            value={query}
-            onChange={onChange}
+              name='search'
+              type='text' 
+              placeholder='Szukaj filmów'
+              value={query}
+              onChange={onChange}
             ></input>
+            {showClearButton && (
+              <button type='button' onClick={clearInput} className='search-input-btn-clear'>
+                x
+              </button>
+            )}
           </div>
           <div className='search-results-container'>
             {results.length > 0 ? (
